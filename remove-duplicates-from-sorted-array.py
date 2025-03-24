f53c02df2a26b6e8ff1f9ac3d6bd04dd1a2c6474
bradcodecraft <bradcodecraft@gmail.com>
2025-03-24 14:10:38 -0400

    solved

--- remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array.py
@@ -2,6 +2,14 @@ from typing import List
 
 
 def removeDuplicates(nums: List[int]) -> int:
-    unique_nums: List[int] = sorted(set(nums))
-    nums[: len(unique_nums)] = unique_nums
-    return len(unique_nums)
+    if not nums:
+        return 0
+
+    write_index: int = 1
+
+    for read_index in range(1, len(nums)):
+        if nums[read_index] != nums[read_index - 1]:
+            nums[write_index] = nums[read_index]
+            write_index += 1
+
+    return write_index
