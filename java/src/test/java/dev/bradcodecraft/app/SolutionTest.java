25e504d4c1f2124d3eadea070ffed54ce864d4b2
bradcodecraft <bradcodecraft@gmail.com>
2025-03-24 19:46:18 -0400

    solved

--- java/src/test/java/dev/bradcodecraft/app/SolutionTest.java
+++ java/src/test/java/dev/bradcodecraft/app/SolutionTest.java
@@ -1,5 +1,6 @@
 package dev.bradcodecraft.app;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -15,5 +16,16 @@ public class SolutionTest {
   void testFindNumbers() {
     assertEquals(2, new Solution().findNumbers(new int[] { 12, 345, 2, 6, 7896 }));
     assertEquals(1, new Solution().findNumbers(new int[] { 555, 901, 482, 1771 }));
+  }
+
+  @Test
+  void testSortedSquares() {
+    assertArrayEquals(
+        new int[] { 0, 1, 9, 16, 100 },
+        new Solution().sortedSquares(new int[] { -4, -1, 0, 3, 10 }));
+
+    assertArrayEquals(
+        new int[] { 4, 9, 9, 49, 121 },
+        new Solution().sortedSquares(new int[] { -7, -3, 2, 3, 11 }));
   }
 }
