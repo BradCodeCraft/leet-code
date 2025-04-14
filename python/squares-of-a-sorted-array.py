8288454e3e30e1950c6c7f7898419ba7baec09ed
bradcodecraft <bradcodecraft@gmail.com>
2025-04-14 14:04:19 -0400

    solved

--- python/squares-of-a-sorted-array.py
+++ python/squares-of-a-sorted-array.py
@@ -2,9 +2,22 @@ from typing import List
 
 
 def sortedSquares(nums: List[int]) -> List[int]:
-    result_array: List[int] = []
+    length: int = len(nums)
+    left_pointer: int = 0
+    right_pointer: int = length - 1
+    result_array: List[int] = [0] * length
 
-    for num in nums:
-        result_array.append(num * num)
+    for i in range(length - 1, -1, -1):
+        if abs(nums[right_pointer]) > abs(nums[left_pointer]):
+            result_array[i] = nums[right_pointer] * nums[right_pointer]
+            right_pointer -= 1
+        else:
+            result_array[i] = nums[left_pointer] * nums[left_pointer]
+            left_pointer += 1
 
-    return sorted(result_array)
+    return result_array
+
+
+print(sortedSquares([-4, -1, 0, 3, 10]))
+print(sortedSquares([-7, -3, 2, 3, 11]))
+print(sortedSquares([1, 4, 9, 25]))
